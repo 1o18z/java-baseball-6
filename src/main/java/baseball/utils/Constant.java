7f289353ce793f81163b21c6b7f8b0a11b714a50
박은지 <qkrdmswl1018@naver.com>
2023-11-19 00:56:43 +0900

    refactor: 상수 클래스 작성

---
+++ src/main/java/baseball/utils/Constant.java
@@ -0,0 +1,24 @@
+package baseball.utils;
+
+public class Constant {
+
+    public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String CONTINUE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String CORRECT_ALL = "3스트라이크 3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String NOTHING = "낫싱";
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+
+    public static final String INVALID_INPUT_LENGTH = "잘못된 입력입니다. 중복되지 않은 세 자리 숫자를 입력해주세요.";
+    public static final String INVALID_RESTART_OPTION_NUMBER = "잘못된 입력입니다. 1(재시작) 또는 2(종료)를 입력해주세요.";
+
+    public static final int DEFAULT_VALUE = 0;
+    public static final int NUMBER_LENGTH = 3;
+    public static final int FULL_CORRECT_COUNT = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+
+    public static final int RESTART = 1;
+    public static final int EXIT = 2;
+}
